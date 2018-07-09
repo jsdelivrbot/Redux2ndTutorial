6cb1c7d4c9aa7e29783d852ff2273b4bb114dc62
BryanRattanavong <arandomexample@hotmail.com>
2018-07-09 14:29:08 -0400

    62

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API_KEY = '2f89f401a4a609e38cce2a6f9a12915e';
-const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid='+ API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
